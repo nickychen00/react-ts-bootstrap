4d2e4f9bf1b572cbe80606b1f751eff1de269f59
adminuser <adminuser@Seans-macbook-pro.local>
2024-05-24 17:24:23 +0800

    排版練習2-2 Custom Components/Pricing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import {GridThemeProvider} from 'bear-react-grid';
 import gridConfig from './config/grid';
+import Pricing from "@/views/Snippets/Pricing";
 // import Headers from './views/Snippets/Headers';
 // import Heroes from './views/Snippets/Heroes';
 // import Features from './views/Snippets/Features';
 // import Sidebars from './views/Snippets/Sidebars';
-import Album from './views/Snippets/Album';
+// import Album from './views/Snippets/Album';
+import Pricingg from './views/Snippets/Album';
+
 
 function App() {
 
   return (
     <GridThemeProvider gridTheme={gridConfig}>
-        <Album/>
+        <Pricingg/>
     </GridThemeProvider>
 );
 }
---
+++ src/views/Snippets/Pricing/Pricing.tsx
@@ -0,0 +1,20 @@
+import {Flex, Container, media, Row, Col, Grid} from 'bear-react-grid';
+import styled, {css} from "styled-components";
+import React from "react";
+
+
+
+
+const Pricingg = () => {
+    return (
+        <PricinggRoot>
+
+        </PricinggRoot>
+    );
+}
+
+
+export default Pricingg;
+
+const PricinggRoot = styled.div`
+`;
