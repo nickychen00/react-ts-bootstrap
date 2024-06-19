3acff08b5afef950997ff5145239a62c8fb03339
imagine10255 <imagine10255@gmail.com>
2024-06-19 15:08:00 +0800

    add swc-react, debug styled-component displayName

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      plugins: [[
+        '@swc/plugin-styled-components', {
+          'displayName': true,
+          'ssr': false
+        }
+      ]],
+    }),
+  ],
 })
